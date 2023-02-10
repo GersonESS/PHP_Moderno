908cebeb9d2e03d744ec482f002c41f46df8fa13
Gerson Santos <gersones@outlook.com>
2023-02-09 21:20:06 -0300

    a

--- ex000/index.php
+++ ex000/index.php
@@ -1,24 +1,7 @@
 <?php
 
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-
-    $nome      = "gustavo";
+    $nome     = "Gustavo";
     $sobrenome = "Guanabara";
-    echo "Muito Prazer , Snome ";
+    echo "Muito Prazer ,  $nome  $sobrenome ";
+ 
 
-    
-</body>
-</html>
-
-?>
